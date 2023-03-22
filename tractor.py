772c9a5357af087c328566bc301dcfa2bc67124f
Aliaksei Brown <alexeybrown2099@gmail.com>
2023-03-22 17:57:33 +0100

    grid fix, changed the robot to chicken, changed the side-movements of the chick

--- tractor.py
+++ tractor.py
@@ -4,30 +4,48 @@ from pygame.math import Vector2
 
 class Tractor:
     def __init__(self, parent_screen, cell_size):
+
+        self.up = pygame.image.load(r'resources/up.png').convert_alpha()
+        self.down = pygame.image.load(r'resources/down.png').convert_alpha()
+        self.left = pygame.image.load(r'resources/left.png').convert_alpha()
+        self.right = pygame.image.load(r'resources/right.png').convert_alpha()
+
         self.parent_screen = parent_screen
-        self.image = pygame.image.load(r'resources/robot2.png').convert_alpha()
-        self.image = pygame.transform.scale(self.image, (cell_size, cell_size+5))
+        #self.image = pygame.image.load(r'resources/robot3.png').convert_alpha()
+
+        self.up = pygame.transform.scale(self.up, (cell_size, cell_size))
+        self.down = pygame.transform.scale(self.down, (cell_size, cell_size+2))
+        self.left = pygame.transform.scale(self.left, (cell_size+2, cell_size+2))
+        self.right = pygame.transform.scale(self.right, (cell_size+3, cell_size+1))
+
+
         self.x = cell_size*2
         self.y = cell_size*2
         self.pos = Vector2(self.x, self.y)
         self.angle = 0
         self.direction = 'up'
+        self.image = self.down
+
 
     def draw(self):
-        self.parent_screen.blit(pygame.transform.rotate(self.image, self.angle), (self.x, self.y))  # rotate tractor
+        self.parent_screen.blit(self.image, (self.x, self.y))  # rotate tractor
 
     def move(self, direction, cell_size):
         if direction == 'up':
             self.y -= cell_size
+            self.image = self.up
             #self.angle = 0
         if direction == 'down':
             self.y += cell_size
+            self.image = self.down
             #self.angle = 180
         if direction == 'left':
             self.x -= cell_size
+            self.image = self.left
             #self.angle = 90
         if direction == 'right':
             self.x += cell_size
+            self.image = self.right
             #self.angle = 270
 
     def walk(self):
